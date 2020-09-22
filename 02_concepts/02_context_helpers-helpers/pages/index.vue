72978abed552fa7775038a9bc2a5ed27a4495bf4
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-09-22 15:32:26 +0200

    fix: generate commands

--- 02_concepts/02_context_helpers-helpers/pages/index.vue
+++ 02_concepts/02_context_helpers-helpers/pages/index.vue
@@ -4,6 +4,8 @@
     <span v-if="$nuxt.isOffline">we are offline &#128556</span>
     <span v-if="$nuxt.isOnline">we are online &#128512</span>
     </h1>
+    <p>Hint: In the network tab swithch to Offline to see the offline message</p>
+
     <p>I am rendered on the {{ renderedOn }} site </p>
     <div>
       <p>{{ content }}</p>
