76fb3e836b781bec0ee8e3151fc2b365ed9ef154
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-09-22 11:31:45 +0200

    fix: update rendering modes

--- 02_concepts/02_context_helpers-helpers/nuxt.config.js
+++ 02_concepts/02_context_helpers-helpers/nuxt.config.js
@@ -1,30 +1,28 @@
 export default {
-    /*
-    ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
-    */
-    components: true,
+  /*
+   ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
+   */
+  components: true,
 
-    /*
-    ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
-    */
-    css: [
-        '~/assets/main.css'
-    ],
+  /*
+   ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
+   */
+  css: ['~/assets/main.css'],
 
-    /*
-    ** Rendering Modes - https://nuxtjs.org/guides/features/rendering-modes
-    */
-    mode: 'spa',
+  /*
+   ** Rendering Modes - https://nuxtjs.org/guides/features/rendering-modes
+   */
+  ssr: 'false',
 
-    /*
-    ** Head Property - https://nuxtjs.org/guides/features/meta-tags-seo
-    */
-    head: {
-        bodyAttrs: {
-            class: 'bodyAttrs bodyAttrs2'
-        },
-        htmlAttrs: {
-            class: 'htmlAttrs'
-        }
-    }    
+  /*
+   ** Head Property - https://nuxtjs.org/guides/features/meta-tags-seo
+   */
+  head: {
+    bodyAttrs: {
+      class: 'bodyAttrs bodyAttrs2',
+    },
+    htmlAttrs: {
+      class: 'htmlAttrs',
+    },
+  },
 }
