ddc5bf02368c098dd7ccd23f493d6e2a34ef1375
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-09-28 13:00:51 +0200

    fix: generate property

--- 04_directory_structure/05_dist/nuxt.config.js
+++ 04_directory_structure/05_dist/nuxt.config.js
@@ -1,23 +1,21 @@
 export default {
-    /*
-    ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
-    */
-    components: true,
-    
-    /*
-    ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
-    */
-    css: [
-        '~/assets/main.css'
-    ],
+  /*
+   ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
+   */
+  components: true,
 
-    /*
-    ** export property
-    */
-    export: {
-        subFolders: false,
-        fallback: '404.html',
-        exclude: [ /admin/ ],
-        // dir: 'my-dist',
-      }  
+  /*
+   ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
+   */
+  css: ['~/assets/main.css'],
+
+  /*
+   ** generate property
+   */
+  generate: {
+    subFolders: false,
+    fallback: '404.html',
+    exclude: [/admin/],
+    // dir: 'my-dist',
+  },
 }
