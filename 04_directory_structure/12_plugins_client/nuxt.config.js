56ce2294affc0109dccb6e4854dca6af3702f1c7
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-09-29 16:49:37 +0200

    fix: improve client only

--- 04_directory_structure/12_plugins_client/nuxt.config.js
+++ 04_directory_structure/12_plugins_client/nuxt.config.js
@@ -1,20 +1,16 @@
 export default {
-    /*
-    ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
-    */
-    components: true,
+  /*
+   ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
+   */
+  components: true,
 
-    /*
-    ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
-    */
-    css: [
-        '~/assets/main.css',
-    ],
+  /*
+   ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
+   */
+  css: ['~/assets/main.css'],
 
-    /*
-    ** Plugins - https://nuxtjs.org/guides/directory-structure/plugins
-    */
-    plugins: [
-        { src: '~/plugins/client-only.js', mode: 'client'}
-    ]
+  /*
+   ** Plugins - https://nuxtjs.org/guides/directory-structure/plugins
+   */
+  plugins: [{ src: '~/plugins/client-only.client.js' }],
 }
