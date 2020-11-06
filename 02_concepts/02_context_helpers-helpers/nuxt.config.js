67b8fdba4aba40560f181539a5dff2eff002981a
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-11-06 10:13:58 +0100

    fix: links to new docs

--- 02_concepts/02_context_helpers-helpers/nuxt.config.js
+++ 02_concepts/02_context_helpers-helpers/nuxt.config.js
@@ -1,16 +1,16 @@
 export default {
   /*
-   ** Activate components - https://nuxtjs.org/guides/directory-structure/components#components-module
+   ** Activate components - https://nuxtjs.org/docs/2.x/directory-structure/components#components-module
    */
   components: true,
 
   /*
-   ** Global CSS - https://nuxtjs.org/guides/features/configuration#pre-processors
+   ** Global CSS - https://nuxtjs.org/docs/2.x/features/configuration#pre-processors
    */
   css: ['~/assets/main.css'],
 
   /*
-   ** Rendering Modes - https://nuxtjs.org/guides/features/rendering-modes
+   ** Plugins - https://nuxtjs.org/docs/2.x/directory-structure/plugins
    */
-  ssr: false,
+  plugins: ['plugins/nuxt-ready.client.js']
 }
